a63e28ba136e036db4b2a16b5fd454152ce79c29
jakub0301 <jakub0301sw@gmail.com>
2018-08-18 14:45:30 +0200

    StarWars Soundboard app

--- Soundboard/ViewController.swift
+++ Soundboard/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    let soundFilenames = ["emp_learntoknow","obiwan_chosenone","swvader01","yoda_twisted"]
+    var audioPlayers = [AVAudioPlayer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for sound in soundFilenames {
+            do{
+                //Try to do
+                let url = NSURL(fileURLWithPath: Bundle.main.path(forResource: sound, ofType: "mp3")!)
+                let audioPlayer = try AVAudioPlayer(contentsOf: url as URL)
+                audioPlayers.append(audioPlayer)
+            }
+            catch{
+                //Catch the error
+                audioPlayers.append(AVAudioPlayer())
+            }
+
+
+            //let audioPlayer = AVAudioPlayer(contentsOf: url)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        //Get the audioPlayer that corresponds to the button thath they tapped
+        let audioPlayer = audioPlayers[sender.tag]
+        audioPlayer.play()
+    }
+    
+    
 }
 
